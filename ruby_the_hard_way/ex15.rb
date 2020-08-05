9080bce8c9d85d4ee8d0ac153a8e53f98eb44c60
Gurinder <gurinder.narang88@gmail.com>
2020-08-05 22:08:14 +0530

    Chapter 15th done

--- ruby_the_hard_way/ex15.rb
+++ ruby_the_hard_way/ex15.rb
@@ -1,12 +1,20 @@
+#Get file name from arguments from command line
 file_name = ARGV.first
 
+# open file using open function
 txt = open(file_name);
 
 puts "Here's your file #{file_name}:"
+# Read file oepn above
 puts txt.read
+txt.close
 
 puts "Type another file name"
+# Get another file name from the user
 file_name_by_user = $stdin.gets.chomp
 
+#Open file usinf file name entered by user
 txt = open(file_name_by_user)
+#Read file enter by user
 puts txt.read
+txt.close
