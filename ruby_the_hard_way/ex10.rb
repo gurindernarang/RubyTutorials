fdc352ccdf7c29df3316bdcd52910a35d7668449
Gurinder <gurinder.narang88@gmail.com>
2020-08-05 19:27:40 +0530

    Chapter 10th done

---
+++ ruby_the_hard_way/ex10.rb
@@ -0,0 +1,24 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+puts "Escape sequence \a"
+puts "Escape sequence \b backslash b"
+puts "Escape sequence \f backslash f"
+puts "Escape sequence \n backslash n"
+puts "Escape sequence \r backslash r"
+puts "Escape sequence \t backslash t"
+puts "Escape sequence \u2665"
+puts "Escape sequence \v backslash v"
+
