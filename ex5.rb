ab28d45256dae4d6e28b2a351536ba4779163f6a
Gurinder <gurinder.narang88@gmail.com>
2020-08-05 13:50:49 +0530

    Fifth chapter done

---
+++ ex5.rb
@@ -0,0 +1,7 @@
+name = "Ruby"
+birth = 1995
+founder_of_ruby = "Yokihiro Matsumoto"
+
+puts "Language we are going to discuss is #{name}"
+puts "#{name} was introduced in #{birth}, by #{founder_of_ruby}"
+
