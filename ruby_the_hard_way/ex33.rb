a6b1ac8f25c7259395ddeb57281a0fd161812a1a
Gurinder <gurinder.narang88@gmail.com>
2020-08-10 13:45:30 +0530

    Chapter 33rd done

--- ruby_the_hard_way/ex33.rb
+++ ruby_the_hard_way/ex33.rb
@@ -1,7 +1,8 @@
 i = 0
 numbers = []
+upto = 10
 
-while i < 6
+while i < upto
   puts "At the top i is #{i}"
   numbers.push(i)
 
@@ -10,6 +11,14 @@ while i < 6
   puts "At the bottom i is #{i}"
 end
 
+for j in (0..10)
+  puts "At the top j is #{j}"
+  numbers.push(j)
+
+  j += 1
+  puts "Numbers now: ", numbers
+  puts "At the bottom j is #{j}"
+end
 puts "The numbers: "
 
 numbers.each { |number| puts number }
